85475f0242dafe4b766199fde9cd92f24a1f6ae1
zachty <zachbyerley@gmail.com>
2022-05-17 00:43:45 -0400

    completed simple button clicking website

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+//classes to be used from html file
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+//function to open modal for event listeners
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+//function to close modal for event listeners
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+//what happens when a modal is clicked
+for (let button of btnsOpenModal) button.addEventListener("click", openModal);
+
+//clicking x button or outside modal box closes the modal
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+//click esc key event
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
